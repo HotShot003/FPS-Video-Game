05b045a4608ace1abf69adaa714338ab143ebf61
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-05-03 01:32:49 +0530

    Working On GameMap(Day)1

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame as pg
 import sys
 from settings import *
+from map import *
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen=pg.display.set_mode(RES)  #Initialize a window or screen for display
         self.clock=pg.time.Clock()  #create an object to help track time
+        self.new_game()
         
     def new_game(self):    
-        pass
+        self.map=Map(self)
     
     def update(self):
         pg.display.flip() #Update the full display Surface to the screen
@@ -18,6 +20,7 @@ class Game:
         
     def draw(self):
         self.screen.fill('black')
+        self.map.draw()
         
     def check_events(self):
         for event in pg.event.get():
--- settings.py
+++ settings.py
@@ -3,3 +3,6 @@
 RES=WIDTH,HEIGHT=1600,900
 FPS = 60
 # print(RES)
+
+
+
