c9a25b0626f8f28b3b56a3e2843c2d2ead9f7b53
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-05-04 02:39:12 +0530

    Add player movement and wall collision
    Implemented player movement using the keyboard keys (a, s, d, w) to control the player's position. Added collision detection with walls to prevent the player from moving through obstacles.
    
    - Player movement: Use 'a' (left), 'd' (right), 'w' (up), 's' (down) keys to move the player.
    - Wall collision: Player's movement is restricted when colliding with map walls.
    (Day 2)

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import pygame as pg
 import sys
 from settings import *
 from map import *
+from player import *
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen=pg.display.set_mode(RES)  #Initialize a window or screen for display
         self.clock=pg.time.Clock()  #create an object to help track time
+        self.delta_time=1
         self.new_game()
         
     def new_game(self):    
         self.map=Map(self)
+        self.player = Player(self)
     
     def update(self):
+        self.player.update()
         pg.display.flip() #Update the full display Surface to the screen
-        self.clock.tick(FPS) #update the clock
+        self.delta_time = self.clock.tick(FPS) #update the clock
         pg.display.set_caption(f'{self.clock.get_fps():.1f}')  #compute the clock framerate
         
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw()
         
     def check_events(self):
         for event in pg.event.get():
