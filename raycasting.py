c05700abef8c0ee24838b124280bf32288040066
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-05-07 00:58:15 +0530

    Implement pseudo 3D projection for realistic wall rendering
    In this commit:
    - Implemented pseudo 3D projection technique to create a 3D-like game map.
    - Developed algorithm to render walls with depth and perspective, enhancing visual realism.
    - Removed fishbowl effect from the rendered walls for a more natural and immersive experience.
    - Adjusted rendering parameters and perspective calculations to achieve optimal visual results.
    
    This change enhances the visual quality and realism of the game map by introducing pseudo 3D projection techniques for rendering walls with depth and perspective, while addressing the fishbowl effect for a more natural viewing experience.
    (Day 4 )

--- raycasting.py
+++ raycasting.py
@@ -63,9 +63,21 @@ class RayCasting:
             
             # draw for debug
             
-            pg.draw.line(self.game.screen , 'yellow',(100 * ox, 100 * oy),
-                         (100 * ox + 100 * depth * cos_a, 100 * oy +100 * depth *sin_a),2)  
-               
+            # pg.draw.line(self.game.screen , 'yellow',(100 * ox, 100 * oy),
+            #              (100 * ox + 100 * depth * cos_a, 100 * oy +100 * depth *sin_a),2)  
+             
+            # Removing Fish Bowl Effect 
+            
+            depth *= math.cos(self.game.player.angle - ray_angle)
+            
+            
+            # Projection 
+            proj_height = SCREEN_DIST / (depth + 0.0001)
+            
+            # Draw WAlls:
+            
+            color = [255 / (1 + depth ** 5 * 0.00002)] * 3
+            pg.draw.rect(self.game.screen,color,(ray * SCALE , HALF_HEIGHT - proj_height //2 , SCALE , proj_height))
                         
 
             ray_angle += DELTA_ANGLE
