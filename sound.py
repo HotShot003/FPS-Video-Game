1242ec92f8444a24ff596e6b00ac3d17c53390c4
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-05-20 23:33:40 +0530

    Few Chnages in Sounds

--- sound.py
+++ sound.py
@@ -11,5 +11,5 @@ class Sound:
         self.npc_pain = pg.mixer.Sound(self.path + 'man-death-scream.mp3')
         self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
         self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
-        self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
+        self.player_pain = pg.mixer.Sound(self.path + 'male-scream-in-fear.mp3')
         self.theme = pg.mixer.Sound(self.path + 'theme.mp3')
